131f4ce5350513d00eec61c7640d8da670e4146a
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 03:24:57 -0700

    Hot fix

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strcat - joins 2 strings
+ * @dest: the first string
+ * @src: the second string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < *dest[i]; i++)
+	{
+	}
+	char string1[i] = *dest;
+	int l;
+
+	for (l = 0; l < *src[l]; l++)
+	{
+	}
+	char string2[l] = *src;
+	int x;
+	char y[l + i];
+
+	for (x = 0; x < l + 1; x++)
+	{
+		y += string2[x];
+	}
+
+	char *concat = &y;
+
+	return (concat);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo contains basic functions I wrote to learn C
